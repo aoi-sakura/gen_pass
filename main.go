01511ba4bdbb37c34f3a04c6be252fb5cd77ff84
wataru <wataru@aoisakura.jp>
2018-03-04 22:33:19 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+	"strconv"
+
+	cryptorand "crypto/rand"
+	"encoding/binary"
+	"math/rand"
+)
+
+const (
+	baseLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	briefSymbols = ".-_@"
+	subSymbols = "!#$%&()*+,/;<=>?[]^`{|}~"
+
+	defaultLength = 16
+)
+
+var (
+	lengthOpt = flag.Int("i", defaultLength, "password length.")
+	noSymbolsOpt = flag.Bool("s", false, "generate password without all symbols.")
+	onlyBriefSymbolsOpt = flag.Bool("S", false, "generate password without some symbols.")
+)
+
+func getNumOfSquare(n int) uint {
+	var i uint = 0
+	for i = 0; n >= 1; i++ {
+		n >>= 1
+	}
+	return i
+}
+
+func randSeed() int64 {
+	var seed int64
+	err := binary.Read(cryptorand.Reader, binary.LittleEndian, &seed)
+	if err != nil {
+		panic(err)
+	}
+	return seed
+}
+
+func generatePassword(n int, letters string) string {
+	bit := getNumOfSquare(len(letters))
+	var mask int64 = 1<<bit - 1
+	var counterMax uint = 63 / bit
+	var randomCounter uint = 0
+	var randomVal int64
+
+	b := make([]byte, n)
+	for i := n - 1; i >= 0; {
+		if randomCounter == 0 {
+			randomVal = rand.Int63()
+			randomCounter = counterMax
+		}
+		index := int(randomVal & mask)
+		if index < len(letters) {
+			b[i] = letters[index]
+			i--
+		}
+		randomVal >>= bit
+		randomCounter--
+	}
+
+	return string(b)
+}
+
+func main() {
+	letters := baseLetters
+	length := defaultLength
+
+	flag.Parse()
+	rand.Seed(randSeed())
+
+	if flag.Arg(0) != "" {
+		lengthValue, err := strconv.Atoi(flag.Arg(0))
+		if err != nil {
+			lengthValue = defaultLength
+		}
+		length = lengthValue
+	} else {
+		length = *lengthOpt
+	}
+
+	if *onlyBriefSymbolsOpt {
+		letters = letters + briefSymbols
+	} else if ! *noSymbolsOpt {
+		letters = letters + briefSymbols + subSymbols
+	}
+
+	fmt.Println(generatePassword(length, letters))
+}
